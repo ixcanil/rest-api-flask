8c3dadc6c63e077796fa2386f742f6c728f3e9b8
Yuna <ixcanil.esime@gmail.com>
2021-02-24 15:49:07 -0600

    with sqlalchemy

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
 from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from flask_cors import CORS
 
 app = Flask(__name__) 
 #BASE DE DATOS
 app.config['SQLALCHEMY_DATABASE_URI']= 'mysql://root@localhost:3306/productos'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+
+CORS(app)
 
 db = SQLAlchemy(app)
 
@@ -35,22 +38,28 @@ products_schema=ProductSchema(many=True)
 
 #from products import products
 
-@app.route('/products')
+@app.route('/ping')
 def ping():
     return jsonify({"message":"pong"})
 
-# @app.route('/products')
-# def getProducts():
-#     return jsonify(products)
+#Obtener productos
+@app.route('/products')
+def getProducts():
+    all_products = Productos.query.all()
+    result = products_schema.dump(all_products)
+    return jsonify(result)
 
-@app.route('/products/<string:product_name>')
-def getProduct(product_name):
+#Obtener solo un producto
+@app.route('/products/<id>')
+def getProduct(id):
+    product = Productos.query.get(id)
+    return product_schema.jsonify(product)
     # product_found=[product for product in products if product['name']==product_name]
     # if (len(product_found)>0):
     #     return jsonify({"product": product_found[0]})
     # else:
-         return jsonify({"message":"Product not found"})
 
+#Agregar Producto
 @app.route('/products',methods=['POST'])
 def addProduct():
     name = request.json['name']
@@ -67,23 +76,30 @@ def addProduct():
     # }
     # products.append(new_product)
      
-
-@app.route('/products/<string:product_name>',methods=['PUT'])
-def editProduct(product_name):
+#Actualizar productos
+@app.route('/products/<id>',methods=['PUT'])
+def editProduct(id):
+    product = Productos.query.get(id)
+    product.cantidad = request.json['quantity']
+    db.session.commit()
+    return product_schema.jsonify(product)
     # product_found=[product for product in products if product['name']==product_name]
     # if len(product_found) > 0:
     #     product_found[0]['name']=request.json['name']
     #     product_found[0]['price']=request.json['price']
     #     product_found[0]['quantity']=request.json['quantity']
     #     return jsonify({'message':'succesfully',"product":product_found})
-    return jsonify({'message':'product not found'})
 
-@app.route('/products/<string:product_name>',methods=['DELETE'])
-def deleteProduct(product_name):
+#Eliminar un producto
+@app.route('/products/<id>',methods=['DELETE'])
+def deleteProduct(id):
+    product = Productos.query.get(id)
+    db.session.delete(product)
+    db.session.commit()
+    return product_schema.jsonify(product)
     # product_found=[product for product in products if product['name']==product_name]
     # if len(product_found) > 0:
     #     products.remove(product_found[0])
-        return jsonify(products)
 
 if __name__=='__main__':
     app.run(debug=True, port=4000) 
